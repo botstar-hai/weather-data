05d6315b69f51b22fd9cafc94a339cf73d928c18
Hai Le Tan <37065046+botstar-hai@users.noreply.github.com>
2019-11-15 14:47:02 +0700

    Update scripting-block-weather.js

--- scripting-block-weather.js
+++ scripting-block-weather.js
@@ -1,13 +1,9 @@
+// NOTE: To use this script correctly,
+// you need to make sure that the bot have to have a flow variable name `foundCity`,
+// which is used for marking the city user entered whether is found or not
+
 const URL = require('url').URL;
 const request = require('request');
-
-// message will be sent when weather can not be found in place user provided
-const notFoundWeatherMessage = 'Sorry, the weather cannot be predicted';
-// Button text to come back share locale block when it was clicked on
-const sendLocationAgainMessage = "Send Location Again";
-
-// share locale block id
-const locationBlockId = 'sbb000aa3-1c4c-4d1f-8b04-77e7f144d445';
 
 /**
  * Fetch forecast data from openweathermap service
@@ -53,9 +49,9 @@ const parseForecastMessages = async (data) => {
     const weather = weathers[String(id)];
     const title = forecast.dt_txt;
     const desc =
-`${name}, ${country} - ${temp}F
-${temp_min}F - ${temp_max}F
-${weather.desc}
+`${name}, ${country} - ${temp}F\n
+${temp_min}F - ${temp_max}F\n
+${weather.desc}\n
 Humidity ${humidity}%`;
     const message = {
       "title": title,
@@ -66,33 +62,25 @@ Humidity ${humidity}%`;
   }).slice(0, 10);
 
   return { cards };
-  //*/
 };
-
-const getFallbackMessage = () => {
-  const shareLocationButton = {
-    title: sendLocationAgainMessage,
-    value: sendLocationAgainMessage,
-    payload: {
-      goToBlock: locationBlockId
-    }
-  };
-  return {
-    text: notFoundWeatherMessage,
-    buttons: [
-      shareLocationButton
-    ]
-  };
-}
 
 // Main Process
 (async function () {
   const weather = await getWeatherData();
-  const message = String(weather.cod) === '200' ? await parseForecastMessages(weather) : getFallbackMessage();
+  const foundCity = String(weather.cod) === '200';
+  const message = foundCity ? await parseForecastMessages(weather) : {};
 
   // More details at https://docs.botstar.com/en/scripting.html#done
   var response = {
-    messages: [].concat(message)
+    messages: [].concat(message),
+    actions: [
+      {
+        type: 'set_variable',
+        data: {
+          foundCity: foundCity ? '1' : '0'
+        }
+      }
+    ]
   };
   done(response); // This is required.
 })()
